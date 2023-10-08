360b8dc753f660c5579da07298897d150d69424e
Ashutosh Singh <ash9926157103@gmail.com>
2023-10-08 17:39:20 +0530

    Updated package.json and Todolist component

--- src/Component/Todolist.jsx
+++ src/Component/Todolist.jsx
@@ -1,51 +1,117 @@
-//To-Do-List-APP
-import React, { useState } from 'react'
-import '../Component/CssFile/Todolist_style.css'
+import React, { useState } from 'react';
+import '../Component/CssFile/Todolist_style.css';
+import { v4 as uuidv4 } from 'uuid';
+import { FaTrash, FaEdit, FaCheck, FaTimes } from 'react-icons/fa';
+
+
 function Todolist() {
-    const [inputValue, setInputValue] = useState('');
-    const [items, setItems] = useState([]);
+    const [userInput, setuserInput] = useState('') //empty string - value of input 
+    const [Items, setItems] = useState([]) // emptyp array - will become the value.. 
 
-    const handleInput = (e) => {
-        setInputValue(e.target.value)
-    }
-    const submitForm = (event) => {
+
+    let itemNumber = 1; // Variable to keep track of the number
+
+    function CrudFun(event) {
         event.preventDefault();
-
-        if (inputValue.trim() === '') {
-            console.log('submit')
+        if (userInput.trim() === '') {
             return;
-        }
-        else {
-            setItems([...items, inputValue])
-            setInputValue('');
-            console.log('else working')
-        }
+        } else {
+            setItems((currentItems) => {
 
+                const newList = { id: uuidv4(), text: userInput, completed: false }
+                const updatedListData = [...currentItems, newList];
+
+                setuserInput('');
+
+                return updatedListData;
+            })
+        }
     }
 
-    return (<>
-        <div className="background">
+    function removeLiElement(id) {
 
-            <div id='pdiv'>
-                <p className='pgraph'>WELCOME TO TODOLIST APP</p>
+        const updatedListData = Items.filter((item) => item.id !== id) //returning the new item after remove the specific id 
+
+        setItems(updatedListData); // assinging the new array items after removing the old items
+
+    }
+    //handle complete
+    function handleComplete(id) {
+        const updatedListData = Items.map((item) => {
+            if (item.id === id) {
+                return { ...item, completed: !item.completed }; //true hai to false or false hai to true 
+            }
+            return item;
+        });
+        setItems(updatedListData);
+    }
+    // edit item
+    function handleEdit(id) {
+        const edit = prompt("hello world", Items.find(item => item.id === id)?.text)
+        if (edit !== null) {
+            const editedListData = Items.map((item) => {
+
+                if (item.id === id) {
+                    return { ...item, text: edit };
+                }
+                return item;
+            });
+            setItems(editedListData);
+        }
+    }
+    //remove all items
+    function removeAll() {
+        setItems([]);
+    }
+
+    return (
+        <>
+            <nav id='mynav'><p id='earth'></p>ToDo-LIST APP</nav>
+
+            <div className="background">
+                <form onSubmit={CrudFun}>
+                    <div className="container">
+                        <input
+                            type='Text'
+                            id='inp'
+                            onChange={(e) => setuserInput(e.target.value)}
+                            value={userInput}
+                            placeholder="Enter a task..."
+                        />
+                        <button id='btn'>Add Details</button>
+                    </div>
+
+                    <div>
+                        <ul id='myul'>
+                            {Items.length > 0 && Items.map((item) => (
+                                <li key={item.id}><span>{itemNumber++}.  {item.text}</span>
+
+                                    <div id='allButtons'>
+                                        <button id='edit' onClick={() => handleEdit(item.id)}><FaEdit /></button>
+
+                                        <button id='complete' onClick={() => handleComplete(item.id)}>
+                                            {item.completed ? <FaCheck /> : <FaTimes />}
+                                        </button>
+
+                                        <button id='del' onClick={() => removeLiElement(item.id)}><FaTrash /></button>
+
+                                    </div>
+                                </li>
+                            ))}
+                            {Items.length >= 1 && (
+                                <div className='center' >
+                                    <button id='removeAll' onClick={removeAll}>Clear</button>
+                                </div>
+                            )}
+                        </ul>
+                    </div>
+                </form>
             </div>
-            <form onSubmit={submitForm}>
-                <div className="container">
-                    <input type='Text' id='inp' onChange={handleInput} value={inputValue} placeholder="Don't be afraid" />
-                    <button id='btn'>Add Details</button>
-                </div>
-
-                <hr />
-                <div>
-                    <ul id='myul'> {items.map((myloop, index) => {
-                      return  <li key={index}>{myloop}</li>
-                    })}
-                    </ul>
-                </div>
-            </form>
-
-        </div>
-    </>)
+            <footer id="footer">
+                <p>&copy; 2023 MAINTAINED BY ASHUTOSH SINGH</p>
+            </footer>
+        </>
+    )
 }
 
-export default Todolist
+export default Todolist;
